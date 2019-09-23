d33cce4156d15800be94a81308d1f85a133ed413
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-23 22:24:16 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,13 @@
 class Author < ActiveRecord::Base
+<<<<<<< HEAD
     validates :name, presence: true
     validates :email, uniqueness: true
     validates :phone_number, length: { is: 10 }
+=======
+   validates :name, presence: true
+   validates :email, uniqueness: true
+   validates :phone_number, length: { is: 10 }
+
+   
+>>>>>>> a8918a5d7860424c6757ff7be4a53e2318069089
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ActiveRecord::Base
+<<<<<<< HEAD
     validates :title, presence: true
     validates :content, length: { minimum: 100 }
     
    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+=======
+     validates :title, presence: true
+   validates :content, length: { minimum: 250 }
+   validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+>>>>>>> a8918a5d7860424c6757ff7be4a53e2318069089
 end
